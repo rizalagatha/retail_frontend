c365216bbb1f62a9b8ee48506c8fc9aa183608a5
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-04-15 12:05:34 +0700

    Dateline Print SO

--- src/views/transaksi/penjualan/SoPrintView.vue
+++ src/views/transaksi/penjualan/SoPrintView.vue
@@ -15,6 +15,7 @@ interface PrintHeader {
   so_top: number;
   so_ket: string;
   so_sc: string;
+  so_dateline: string | null;
   cus_nama: string;
   cus_alamat: string;
   cus_kota: string;
@@ -137,6 +138,12 @@ onMounted(() => {
           <div>
             <span class="label">Tanggal:</span>
             {{ format(parseISO(printData.header.so_tanggal), "dd-MM-yyyy") }}
+          </div>
+          <div v-if="printData.header.so_dateline">
+            <span class="label text-blue-darken-2">Dateline:</span>
+            <span class="font-weight-bold text-blue-darken-2">
+              {{ format(parseISO(printData.header.so_dateline), "dd-MM-yyyy") }}
+            </span>
           </div>
           <div><span class="label">Tempo:</span> {{ printData.header.so_top }} Hari</div>
           <div><span class="label">Keterangan:</span> {{ printData.header.so_ket }}</div>

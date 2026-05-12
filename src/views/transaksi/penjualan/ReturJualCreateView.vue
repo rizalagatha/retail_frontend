c14659ad701759cc0160123110c00cc3af6435f2
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-05-12 14:19:19 +0700

    retur k01

--- src/views/transaksi/penjualan/ReturJualCreateView.vue
+++ src/views/transaksi/penjualan/ReturJualCreateView.vue
@@ -166,7 +166,7 @@ const onInvoiceSelected = async (invoice: { nomor: string; tanggal: string }) =>
   const isKPR = authStore.user?.cabang === "KPR";
 
   // Flag pembukaan akses sementara untuk K10
-  const isTemporaryOpen = authStore.user?.cabang === "K01" && new Date() < new Date("2026-04-29");
+  const isTemporaryOpen = authStore.user?.cabang === "K01" && new Date() < new Date("2026-05-12");
 
   // Jika bukan KON, bukan K10 (temporary), dan invoice > 1 hari, maka blokir
   if (!isKON && !isKPR && !isTemporaryOpen && hariSejakInvoice > 1) {

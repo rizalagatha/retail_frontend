83121da3bfb8e456cf7add465269863ae57a27bd
Rizal Agatha Erdin Agesyah <rizalagatha1403@gmail.com>
2026-06-30 13:34:20 +0700

    fix

--- src/views/piutang/SetoranBayarCreateView.vue
+++ src/views/piutang/SetoranBayarCreateView.vue
@@ -1056,6 +1056,7 @@ watch(() => header.nominal, calculateTotals);
     <CustomerSearchModal
       v-if="dialog.customerSearch"
       :gudang="authStore.user?.cabang || ''"
+      source="invoice"
       @close="dialog.customerSearch = false"
       @customer-selected="onCustomerSelected"
     />
